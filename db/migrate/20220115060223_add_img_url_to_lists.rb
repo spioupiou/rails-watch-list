ec50997c0555740efe1cb431e383b7606a7bc6f0
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-18 20:25:04 +0900

    change platform on gemfile.lock

--- db/migrate/20220115060223_add_img_url_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddImgUrlToLists < ActiveRecord::Migration[6.0]
-  def change
-    add_column :lists, :img_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_060223) do
+ActiveRecord::Schema.define(version: 2022_01_15_031426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2022_01_15_060223) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "img_url"
   end
 
   create_table "movies", force: :cascade do |t|

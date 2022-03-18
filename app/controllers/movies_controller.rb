113f49b628461fd9f025f13ed1152bb8fecde20e
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-18 19:47:08 +0900

    List style

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,12 +36,12 @@ class MoviesController < ApplicationController
     redirect_to list_path(@list)
   end
 
-  # def destroy
-  #   @bookmark = Bookmark.find(params[:id])
-  #   @list_id = @bookmark.list_id
-  #   @bookmark.destroy
-  #   redirect_to list_path(@list_id)
-  # end
+  def destroy
+    @movie = Bookmark.find(params[:id])
+    @list_id = @bookmark.list_id
+    @bookmark.destroy
+    redirect_to list_path(@list_id)
+  end
 
   private
 
